373b99be651b7d034e596fd3513633a0ce8c06b6
KalpitSancheti <142486799+KalpitSancheti@users.noreply.github.com>
2024-03-28 17:15:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var random1=Math.floor(Math.random()*6)+1;
+//console.log(random1);
+var img1=document.querySelector(".img1");
+
+img1.src="./images/dice"+random1+".png";
+
+var random2=Math.floor(Math.random()*6)+1;
+//console.log(random1);
+var img2=document.querySelector(".img2");
+
+img2.src="./images/dice"+random2+".png"; 
+
+document.querySelector("h1").innerText;
+
+
+
+if(random1==random2){
+    document.querySelector("h1").innerText="🟰 It's A Tie 🟰";
+}
+else if(random1>random2){
+    document.querySelector("h1").innerText="👈 Player 1 Wins!";
+
+}
+else{
+    document.querySelector("h1").innerText="Player 2 Wins! 👉";
+
+}
